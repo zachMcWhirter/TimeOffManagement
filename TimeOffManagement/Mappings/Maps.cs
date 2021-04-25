690b529d419574c03002abffc951159f01cd94e0
Zach McWhirter <zmac7811@gmail.com>
2021-04-25 17:21:52 -0500

    Setup ViewModels and AutoMapper

---
+++ TimeOffManagement/Mappings/Maps.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeOffManagement.Data;
+using TimeOffManagement.Models;
+
+namespace TimeOffManagement.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<TimeOffType, DetailsTimeOffTypeVM>().ReverseMap();
+            CreateMap<TimeOffType, CreateTimeOffTypeVM>().ReverseMap();
+            CreateMap<TimeOffAllocation, TimeOffAllocationVM>().ReverseMap();
+            CreateMap<TimeOffHistory, TimeOffHistoryVM>().ReverseMap();
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+        }
+    }
+}
